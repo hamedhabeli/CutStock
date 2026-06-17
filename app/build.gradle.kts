d79f77e4a3568602c0a6897c880a4c04280fd226
hamed <hamed.habeli@gmail.com>
2026-06-17 20:01:57 +0330

    Migrate Poolakey dependency coordinate to JitPack to bypass DNS blockage in GitHub Actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,7 @@ android {
 }
 
 dependencies {
-    "bazaarImplementation"("ir.cafebazaar.poolakey:poolakey:2.2.0")
+    "bazaarImplementation"("com.github.cafebazaar:Poolakey:2.2.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")

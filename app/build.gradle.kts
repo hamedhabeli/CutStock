7f19eb62e066894362cfc57b21f29378e22af4cf
hamed <hamed.habeli@gmail.com>
2026-06-19 22:18:33 +0330

    Upgrade minSdkVersion to 26 and Crashlytics plugin version to 3.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         applicationId = "ir.cutmize.app"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
